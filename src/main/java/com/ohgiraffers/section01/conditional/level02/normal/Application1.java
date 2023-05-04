49661da5fd6ef65f552addccff7d68e6d99d142b
jongkyuh <jongkyuh@naver.com>
2023-05-04 11:26:33 +0900

    [section01.level2.normal][1번] 완료
    스캐너 객체를 선언해서 정수값을 입력받아 그 값이 홀수면 "홀수다" 를 출력하고 그값이 짝수면 "짝수다"를 출력한다. 단 입력받은 값이 1보다 작거나 10보다 크게되면 "반드시 1~10사이의 정수를 입력해야 합니다." 를 출력합니다.

--- src/main/java/com/ohgiraffers/section01/conditional/level02/normal/Application1.java
+++ src/main/java/com/ohgiraffers/section01/conditional/level02/normal/Application1.java
@@ -1,14 +1,20 @@
 package com.ohgiraffers.section01.conditional.level02.normal;
-
+import java.util.Scanner;
 public class Application1 {
 
 	public static void main(String[] args) {
-		
+
 		/* 1~10 사이의 정수 한개를 입력받아 홀수인지 짝수인지 인지 확인하고,
 		 * 홀수이면 "홀수다.", 홀수가 아니면 "짝수다." 라고 출력하세요.
 		 * 단, 1~10 사이의 정수가 아닌 경우 "반드시 1~10 사이의 정수를 입력해야 합니다." 를 출력하세요. 
 		 * */
-		
+		System.out.print("1부터10사이의 정수를 입력해주세요 : ");
+		Scanner sc = new Scanner(System.in);		//스캐너 객체 선언
+		int a = sc.nextInt();						//입력받은값을 변수 a에 저장
+
+		if(a<1 || a>10) System.out.println("반드시 1~10 사이의 정수를 입력해야 합니다.");	//a에 1~10사이를 벗어나는 값인 경우 출력
+		else if(a%2==0) System.out.println("짝수다");								//a의 값이 짝수일 때 출력
+		else System.out.println("홀수다");											//a의 값이 홀수일 때 출력
 	}
 
 }
