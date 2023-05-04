d39f9f78775b127199c811601c76e47a55a0d8e9
jongkyuh <jongkyuh@naver.com>
2023-05-04 13:29:46 +0900

    [looping_and_branching][basic][3번] 완료
    스캐너 객체를 이용해 정수를 입력받습니다. for 문을 이용해 1부터 입력받은 정수까지의 짝수값을 sum 변수에 누적합니다. for문제에서 짝수를 입력받는 방법은 mod연산자를 통해 홀수임이 나타나면 continue문을 이용해 다시 반복문을 실행합니다.

--- src/main/java/com/ohgiraffers/section02/looping_and_branching/level01/basic/Application3.java
+++ src/main/java/com/ohgiraffers/section02/looping_and_branching/level01/basic/Application3.java
@@ -1,9 +1,9 @@
 package com.ohgiraffers.section02.looping_and_branching.level01.basic;
-
+import java.util.Scanner;
 public class Application3 {
 
 	public static void main(String[] args) {
-		
+
 		/* 1부터 입력받은 정수까지의 짝수의 합을 구하세요
 		 * 
 		 * -- 입력 예시 --
@@ -12,7 +12,18 @@ public class Application3 {
 		 * -- 출력 예시 --
 		 * 1부터 10까지 짝수의 합 : 30
 		 * */
-		
+		int sum = 0;                                    //누적할 변수 초기화
+		Scanner sc = new Scanner(System.in);            //스캐너 객체 선언
+		System.out.print("정수를 입력하세요 : ");
+		int getNum = sc.nextInt();                        //사용자 정수 입력받기
+
+		for(int i = 1;i<=getNum;i++){
+			if(i%2==1) continue;
+			sum+=i;
+		}
+
+		System.out.println("1부터 " + getNum + "까지의 짝수의 합 : " + sum);
+
 	}
 
 }
