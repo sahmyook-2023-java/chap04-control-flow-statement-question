3102aaf4157e0c4144c648870aabbdaa89bbf007
jongkyuh <jongkyuh@naver.com>
2023-05-04 16:01:47 +0900

    [looping_and_branching][advanced][2번] 완료
    낸 금액에서 상품가격을 뺸 값을 restMoney에 초기화 합니다.
    50000권부터 10원동전까지 갯수를 세기위해
    restMoney에서 50000을 나눈 몫이 50000원권의 개수이고 나머지는 50000원권을 제외한 금액이 되므로 restMoney에 50000을 나눈 나머지를 다시 재대입해서 10000을 계산합니다.

--- src/main/java/com/ohgiraffers/section02/looping_and_branching/level04/advanced/Application2.java
+++ src/main/java/com/ohgiraffers/section02/looping_and_branching/level04/advanced/Application2.java
@@ -1,9 +1,9 @@
 package com.ohgiraffers.section02.looping_and_branching.level04.advanced;
-
+import java.util.Scanner;
 public class Application2 {
 
 	public static void main(String[] args) {
-		
+
 		/* 받은 금액을 입력 받고, 상품 가격을 입력 받아 거스름돈을 계산하는 프로그램을 작성하세요
 		 * 
 		 * 단, 거스름돈은 대한민국 화폐 단위 별 필요 수량이 출력되도록 작성하고,
@@ -26,7 +26,61 @@ public class Application2 {
 		 * ============================
 		 * 거스름돈 : 77660원
 		 * */
-		
+
+		Scanner sc = new Scanner(System.in);
+		int ohmanwon;
+		int manwon;
+		int ohcheonwon;
+		int cheonwon;
+		int ohbaekwon;
+		int baekwon;
+		int ohsipwon;
+		int sipwon;
+		int restMoney = 0;
+
+		System.out.print("받으신 금액을 입력하세요 : ");
+		int giveMoney = sc.nextInt();
+		System.out.print("상품 가격을 입력하세요 : ");
+		int valuePrice = sc.nextInt();
+
+		restMoney = giveMoney-valuePrice;
+
+		ohmanwon = restMoney/50000;
+		restMoney = restMoney%50000;
+
+		manwon = restMoney/10000;
+		restMoney = restMoney % 10000;
+
+		ohcheonwon = restMoney/5000;
+		restMoney = restMoney % 5000;
+
+		cheonwon = restMoney / 1000;
+		restMoney = restMoney % 1000;
+
+		ohbaekwon = restMoney / 500;
+		restMoney = restMoney % 500;
+
+		baekwon = restMoney / 100;
+		restMoney = restMoney % 100;
+
+		ohsipwon = restMoney / 50;
+		restMoney = restMoney % 50;
+
+		sipwon = restMoney / 10;
+		restMoney = restMoney % 10;
+
+		System.out.println("========================================");
+		System.out.println("50000원권 지폐 " + ohmanwon + "장");
+		System.out.println("10000원권 지폐 " + manwon + "장");
+		System.out.println("5000원권 지폐 " + ohcheonwon + "장");
+		System.out.println("1000원권 지폐 " + cheonwon + "장");
+		System.out.println("500원권 동전 " + ohbaekwon + "개");
+		System.out.println("100원권 동전 " + baekwon + "개");
+		System.out.println("50원권 동전 " + ohsipwon + "개");
+		System.out.println("10원권 동전 " + sipwon + "개");
+		System.out.println("거스름돈 : " + restMoney + "원");
+
+
 	}
 
 }
